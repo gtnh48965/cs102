71125617d4e5348bb18c2728b2f6d54420215a6e
gtnh48965 <54045323+gtnh48965@users.noreply.github.com>
2019-10-10 20:29:17 +0300

    Добавлен шаблон для шифра Цезаря

---
+++ caesar.py
@@ -0,0 +1,34 @@
+def encrypt_caesar(plaintext: str) -> str:
+    """
+        Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+
+    
+
+
+def encrypt_caesar(plaintext: str) -> str:
+    """
+        Encrypts plaintext using a Caesar cipher.
+
+    >>> encrypt_caesar("PYTHON")
+    'SBWKRQ'
+    >>> encrypt_caesar("python")
+    'sbwkrq'
+    >>> encrypt_caesar("Python3.6")
+    'Sbwkrq3.6'
+    >>> encrypt_caesar("")
+    ''
+    """
+    # PUT YOUR CODE HERE
+    
+    return plaintext
