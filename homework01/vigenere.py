8d8222bb648e7cc63341e4282e8d8197d25f2230
gtnh48965 <54045323+gtnh48965@users.noreply.github.com>
2019-10-12 03:26:12 +0300

    Корекция 1.1

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -12,24 +12,30 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     # PUT YOUR CODE HE
     ciphertext = " "
     keyword =keyword.lower()
+    nana = ord('a')
+    nanA = ord('A')
+    if nana > nanA:
+        neyn = nana
+    else:
+        neyn = nanA
     
     while len(plaintext) > len(keyword):
         keyword += keyword
     ss = len(plaintext) // len(keyword)
     for i, j in enumerate(plaintext):
         if 'a' <= j <= 'z':
-            key=(ord(keyword[i % len(keyword)]) - 97)%26
-            mid = (ord(j) + key -97)%26 +97
-            ciphertext += chr (mid)
+            key = (ord(keyword[i % len(keyword)]) - neyn) % 26
+            mid = (ord(j) + key - nana) % 26 + nana
+            ciphertext += chr(mid)
         elif 'A' <= j <= 'Z':
-            key = (ord(keyword[i % len(keyword)]) - 97) % 26
-            mid = (ord(j) + key - 65) % 26 + 65
+            key = (ord(keyword[i % len(keyword)]) - neyn) % 26
+            mid = (ord(j) + key - nanA) % 26 + nanA
             ciphertext += chr(mid)
         else:
             ciphertext += j
 
 
-return ciphertext
+    return ciphertext
 
 
 
@@ -47,20 +53,26 @@ def decrypt_vigenere(ciphertext, keyword):
     # PUT YOUR CODE HERE
     plaintext = " "
     keyword = keyword.lower()
-    
+    nana = ord('a')
+    nanA = ord('A')
+    if nana > nanA:
+        neyn = nana
+    else:
+        neyn = nanA
+
     while len(ciphertext) > len(keyword):
         keyword += keyword
-    ss = len(ciphertext) // len(keyword)+1
+    ss = len(ciphertext) // len(keyword)
     for i, j in enumerate(ciphertext):
         if 'a' <= j <= 'z':
-            key = (ord(keyword[i % len(keyword)]) - 97) % 26
-            mid = (ord(j) - key - 97) % 26 + 97
+            key = (ord(keyword[i % len(keyword)]) - neyn) % 26
+            mid = (ord(j) - key - nana) % 26 + nana
             plaintext += chr(mid)
         elif 'A' <= j <= 'Z':
-            key = (ord(keyword[i % len(keyword)]) - 97) % 26
-            mid = (ord(j) - key - 65) % 26 + 65
+            key = (ord(keyword[i % len(keyword)]) - neyn) % 26
+            mid = (ord(j) - key - nanA) % 26 + nanA
             plaintext += chr(mid)
         else:
             plaintext += j
 
-return plaintext
+    return plaintext
